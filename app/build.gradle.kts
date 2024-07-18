367d42326f26e6e3fca1cf31f4ccc2c9029d7eea
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-18 17:34:06 +0300

    начало работы с базой данных через Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,10 @@ android {
 }
 
 dependencies {
-//    implementation("androidx.room:room-runtime:2.5.1")
-//    annotationProcessor("androidx.room:room-compiler:2.5.1")
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
